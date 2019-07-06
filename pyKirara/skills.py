03551f21d7d6c5237f16649643c2ac7891bdd42c
EthanSk13s <ethanmiranda11@gmail.com>
2019-07-06 15:13:08 -0700

    add WIP docs

--- pyKirara/skills.py
+++ pyKirara/skills.py
@@ -1,4 +1,45 @@
 class Skill:
+    """Represents a Skill and its Data
+
+    Attributes
+    ----------
+    id : int
+        The Skill's ID
+    name : str
+        The Skill's name
+    explain : str
+        An untranslated explanation of the skill
+    en_explain : str
+        A machine translated explanation of the skill
+    skill_type : str
+        The Skill's type
+    judge_type : int
+        ????
+    trigger_type : int
+        ????
+    trigger_value : int
+        ????
+    cutin_type : int
+        ????
+    condition : int
+        ????
+    value : int
+        ????
+    value_2 : int
+        ????
+    max_chance : int
+        percent stuff
+    max_duration : int
+        The max duration of how long the skill last in milliseconds
+    skill_type_id : int
+        The skill type's id
+    effect_length : dict
+        A dict of the minimum and max value of the how long the skill's effect
+        last
+    proc_chance : dict
+        A dict of the minimum and max value of the probability the skill will
+        take effect
+    """
     def __init__(self, skill_data: dict):
         self.id = skill_data['id']
         self.name = skill_data['skill_name']
@@ -7,7 +48,6 @@ class Skill:
 
         self.skill_type = skill_data['skill_type']
         self.judge_type = skill_data['judge_type']
-        
         self.trigger_type = skill_data['skill_trigger_type']
         self.trigger_value = skill_data['skill_trigger_value']
 
@@ -23,7 +63,47 @@ class Skill:
         self.effect_length = skill_data['effect_length']
         self.proc_chance = skill_data['proc_chance']
 
+
 class LeadSkill:
+    """Represents a Lead Skill and it's data
+
+    Attributes
+    ----------
+    id : int
+        The Skill's ID
+    name : str
+        The Skill's name
+    explain : str
+        An untranslated explanation of the skill
+    en_explain : str
+        A machine translated explanation of the skill
+    need_cute : bool
+        A bool value if the skill requires a Cute Idol
+    need_cool : bool
+        A bool value if the skill requires a Cool Idol
+    need_passion : bool
+        A bool value if the skill requires a Passion Idol
+    target_attribute : str
+        What attribute the skill affects
+    target_attribute_2 : str
+        Another attribute the skill affects
+    target_param : str
+        What parameter the skill affects (Visual, Vocal, etc.)
+    target_param : str
+        Another parameter the skill affects
+    up_type : int
+        ???
+    up_type_2 : int
+        ???
+    up_value : int
+        ???
+    up_value_2 : int
+        ???
+    special_id : int
+        The skill's special ID
+    need_chara : str
+        If the skill needs a specific character
+    """
     def __init__(self, skill_data: dict):
         self.id = skill_data['id']
         self.name = skill_data['name']
